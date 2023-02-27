d254873ec52314db3ea2f8b3f0093c5bf15ad19e
Yura <temp.yura@gmail.com>
2023-02-27 08:37:18 +0300

    AdDetailSerializer get_locations ad

--- ads/serializers/ad.py
+++ ads/serializers/ad.py
@@ -15,6 +15,10 @@ class AdSerializer(ModelSerializer):
 class AdDetailSerializer(ModelSerializer):
     author = SlugRelatedField(slug_field="username", queryset=User.objects.all())
     category = SlugRelatedField(slug_field="name", queryset=Category.objects.all())
+    locations = SerializerMethodField()
+
+    def get_locations(self, ad):
+        return [location.name for location in ad.author.locations.all()]
 
     class Meta:
         model = Ad
